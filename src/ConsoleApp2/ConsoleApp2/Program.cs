34dfdd002c3d96c65e1c4af9d5081e9972b09caf
Edvard909 <edi-9@inbox.ru>
2023-03-16 18:56:40 +0400

    Employee

---
+++ src/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[2];
+            Employee employee1 = new Employee()
+            {
+                Name = "EDG",
+                Age = 21,
+                Specialty = "3D",
+            };
+
+            Employee employee2 = new Employee()
+            {
+                Name = "Afet",
+                Age = 20,
+                Specialty = "Sculpturing",
+            };
+            Console.WriteLine(employee1.Name + $" {employee1.Age} years old" + $" {employee1.Specialty} Artist");
+            Console.ReadKey();
+        }
+    }
+    public class Employee 
+    { 
+        public string Name { get; set; }
+        public int Age;
+        public string Specialty { get; set; }
+    }
+}
